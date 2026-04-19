1670cd9cfb1bd54e23314e27fc689776a22df67c
unknown <naggu1999@naver.com>
2026-04-20 00:49:54 +0900

    네이버 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
   verification: {
     google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION ?? '',
     other: {
-      'naver-site-verification': [process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION ?? ''],
+      'naver-site-verification': ['aaf6d83f723d68faddb24283da6de1c8400d7b12'],
     },
   },
 }
